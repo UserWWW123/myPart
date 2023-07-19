7d2818991c578bff29beef3cd63f38376fc0d6b6
KL <lamkayin1037@gmail.com>
2023-07-19 18:29:54 -0400

    Initial Commit

---
+++ myPart/myPartApp.swift
@@ -0,0 +1,17 @@
+//
+//  myPartApp.swift
+//  myPart
+//
+//  Created by KWOK LAM on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct myPartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
